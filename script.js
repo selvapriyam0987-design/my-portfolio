20b63ee46fe6c488ea0ab706dd84c8d9118a09b4
Selva Priya <selvapriyam0987@gmail.com>
2026-02-21 12:43:57 +0530

    updated navbar UI

--- script.js
+++ script.js
@@ -70,19 +70,4 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-/*experiance*/
-window.addEventListener("scroll", reveal);
 
-function reveal(){
-  const reveals = document.querySelectorAll(".reveal");
-
-  for(let i=0;i<reveals.length;i++){
-    const windowHeight = window.innerHeight;
-    const elementTop = reveals[i].getBoundingClientRect().top;
-    const elementVisible = 100;
-
-    if(elementTop < windowHeight - elementVisible){
-      reveals[i].classList.add("active");
-    }
-  }
-}
